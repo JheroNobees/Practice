1a9defdda9b0591f710a125da5388f913892d2f2
JheroNobees <74825849+JheroNobees@users.noreply.github.com>
2020-12-06 06:41:33 +0800

    Add files via upload

---
+++ My AwesomeProgram/Practice102/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice102
+{
+    class Practice102
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string Username = Console.ReadLine();
+
+            Console.WriteLine("HI," +Username+ "\nHow are you today?");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("That's good");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ My AwesomeProgram/Practice103/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Num01;
+            Double Num02;
+            Double Num03;
+
+            Console.Write("Input your first number: ");
+            Num01 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input your second number: ");
+            Num02 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input your Third number: ");
+            Num03 = Convert.ToDouble(Console.ReadLine());
+            Double result = (Num01 + Num02 + Num03)/3;
+            Console.WriteLine("YOUR RESULT IS " +result);
+            
+            Console.ReadKey();
+        }
+    }
+}
