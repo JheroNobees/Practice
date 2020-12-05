d44b628726b4e09cdfec5119988b61cef6598737
JheroNobees <74825849+JheroNobees@users.noreply.github.com>
2020-12-06 06:43:38 +0800

    Add files via upload

---
+++ Practice101.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace My_AwesomeProgram
+{
+    class Practice101
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Practice";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+            Console.WriteLine("Hello, your going to Write your own code");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Welcome to Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
